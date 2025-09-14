4296c76c57c05f7f22cdffdd4425cae428ed27ec
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-09-14 20:54:26 +0530

    Add files via upload

---
+++ CWCTH.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin>>A>>B;
+    if(B>=3*A){
+        cout<<"Rain";
+    }
+    else if(B<3*A){
+        cout<<"Dry";
+    }
+    
+}
