570d108f8a1a385fc955bf1e2c9d899824b35925
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-09-28 21:50:38 +0530

    Add files via upload

---
+++ P1169.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x;
+	
+	cin>>x;
+	if(x>=10){
+	    cout<<"YES";
+	    
+	}else{
+	    cout<<"NO";
+	}
+
+}
